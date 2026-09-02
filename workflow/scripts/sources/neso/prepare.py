69753b811f50854d2b9a79740bb565990791f3e8
Dano Hawkins <d.d.a.hawkins@tudelft.nl>
2026-09-02 22:41:40 +0200

    Updated NESO prep, to account for inconsistent date stamps in stource

--- workflow/scripts/sources/neso/prepare.py
+++ workflow/scripts/sources/neso/prepare.py
@@ -16,7 +16,9 @@ def add_utc_timestamps(data: pd.DataFrame) -> pd.DataFrame:
     prepared = data.copy()
 
     prepared["SETTLEMENT_DATE"] = pd.to_datetime(
-        prepared["SETTLEMENT_DATE"], errors="raise"
+        prepared["SETTLEMENT_DATE"],
+        format="mixed",
+        errors="raise",
     ).dt.normalize()
 
     prepared["SETTLEMENT_PERIOD"] = pd.to_numeric(
